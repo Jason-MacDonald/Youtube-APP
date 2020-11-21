8bbf323f3fdfac5a0d4cf03ad64e26a7d0507716
Jason-MacDonald <49774612+Jason-MacDonald@users.noreply.github.com>
2020-11-21 19:57:47 +1300

    Initial Deploy

---
+++ src/components/VideoItem.js
@@ -0,0 +1,15 @@
+import './VideoItem.css';
+import React from 'react';
+
+const VideoItem = ({ video, onSelectVideo }) => {
+    return (
+        <div className="video-item item" onClick={() => onSelectVideo(video)}>
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt='video thumbnail' />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>          
+        </div>
+    )
+};
+
+export default VideoItem;
